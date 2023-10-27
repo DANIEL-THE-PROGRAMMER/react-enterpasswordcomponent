ca0853c3bcc2cdc064697a4b6b4a2aca93b3a6fe
Daniel-TheProgrammer <njid18753@gmail.com>
2023-10-27 16:29:02 +0100

    feat: started stylings✅

---
+++ password_component/src/components/Input/Input.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Input = () => {
+  return <b>Password</b>;
+};
+
+export default Input;
--- password_component/src/components/Login/Login.jsx
+++ password_component/src/components/Login/Login.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Avatar from "../Avatar/Avatar";
+import Input from "../Input/Input";
+import Toggle from "../Toggle/Toggle";
 
 const Login = () => {
   return (
     <main>
       <h1>Enter Your Password</h1>
+      <Avatar />
+      <Input />
+      <Toggle />
     </main>
   );
 };
