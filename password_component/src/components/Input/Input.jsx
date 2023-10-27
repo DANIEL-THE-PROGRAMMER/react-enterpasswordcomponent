5353ea80479f7592b3f4ce4b1df26a79702ceee2
Daniel-TheProgrammer <njid18753@gmail.com>
2023-10-27 18:30:35 +0100

    feat: input styles

--- password_component/src/components/Input/Input.jsx
+++ password_component/src/components/Input/Input.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 const Input = () => {
-  return <b>Password</b>;
+  const [visible, setVisible] = useState(false);
+  const inputRef = useRef(null);
+  const isVisible = () => {
+    setVisible(!visible);
+    inputRef.current.focus();
+  };
+
+  return (
+    <section className="section-input">
+      <b>Password</b>
+      <div className="container">
+        <div className="container_input">
+          <ion-icon name="lock-closed-outline"></ion-icon>{" "}
+          <input type={visible ? "text" : "password"} ref={inputRef} />
+        </div>
+        <button onClick={isVisible}>
+          {visible ? (
+            <ion-icon name="eye-online"></ion-icon>
+          ) : (
+            <ion-icon name="eye-off-outline"></ion-icon>
+          )}
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default Input;
