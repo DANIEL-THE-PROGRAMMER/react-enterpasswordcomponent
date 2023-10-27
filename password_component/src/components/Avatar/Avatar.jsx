c6a72b1b22b6bb11aad7e0ee7fb18cb0aa009005
Daniel-TheProgrammer <njid18753@gmail.com>
2023-10-27 16:13:12 +0100

    feat: some component skeleton

---
+++ password_component/src/components/Avatar/Avatar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Avatar = () => {
+  return (
+    <section>
+      <div>
+        <p>Business Account</p>
+        <b>Sarah Bills</b>
+      </div>
+    </section>
+  );
+};
+
+export default Avatar;
---
+++ password_component/src/components/Toggle/Toggle.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Toggle = () => {
+  return <b>Stay Signed In</b>;
+};
+
+export default Toggle;
